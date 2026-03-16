6f59054b967d5242b98d354749f1caa7f12754ad
Ryszard Zmija <github@ryszardzmija.com>
2026-03-16 14:42:45 +0100

    fix: remove redundant global configuration call

--- src/main/java/com/ryszardzmija/storage/serialization/io/ReadRequest.java
+++ src/main/java/com/ryszardzmija/storage/serialization/io/ReadRequest.java
@@ -5,7 +5,7 @@ import com.ryszardzmija.storage.config.StorageConfigHolder;
 public record ReadRequest(long offset) {
     public ReadRequest {
         long maxSegmentSize = StorageConfigHolder.get().maxSegmentSize();
-        if (offset < 0 || offset > StorageConfigHolder.get().maxSegmentSize()) {
+        if (offset < 0 || offset > maxSegmentSize) {
             throw new IllegalArgumentException("offset must be non-negative and within the maximum size of a segment: " + maxSegmentSize);
         }
     }
