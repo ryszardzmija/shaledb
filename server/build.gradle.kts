af445db6748491f8973418b8a09b07f95496e24f
Ryszard Zmija <github@ryszardzmija.com>
2026-03-18 13:57:26 +0100

    feat: add Protobuf and gRPC dependencies and define API contract

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,11 +1,35 @@
 plugins {
     application
+    id("com.google.protobuf") version "0.9.6"
 }
 
 dependencies {
     implementation(project(":storage"))
+
+    implementation("io.grpc:grpc-netty-shaded:1.79.0")
+    implementation("io.grpc:grpc-protobuf:1.79.0")
+    implementation("io.grpc:grpc-stub:1.79.0")
+    implementation("com.google.protobuf:protobuf-java:4.34.0")
 }
 
 application {
     mainClass.set("com.ryszardzmija.shaledb.server.Application")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:4.34.0"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.79.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+            }
+        }
+    }
 }
